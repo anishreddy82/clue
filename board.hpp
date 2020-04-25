48567bda200b0bbe1aafd55be1a6173e0a953123
anishreddy82 <50680828+anishreddy82@users.noreply.github.com>
2020-04-25 12:58:24 -0700

    Created a visual Board
    Created a visual board.

---
+++ board.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include <vector>
+#include "Player.hpp"
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include "room.hpp"
+
+class Board{
+public:
+	void dif();
+     Room **boardArray;
+};
