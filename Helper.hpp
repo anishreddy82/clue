d284a1772137bb26b24597f9273b5b3a6f354d43
Kevin Benoit <kevin.benoit8@gmail.com>
2020-04-29 08:35:06 -0600

    adding Helper functions

---
+++ Helper.hpp
@@ -0,0 +1,24 @@
+/****************************************************************************
+Name:Helper.hpp
+Author: Kevin Benoit
+Date: 4/22/2020
+Description: Set of helper functions for file IO, string maninuplaction, ect. Will expand as class continues
+Citations:
+Format of Helper files/class:
+CS 162 Group Project, Steven Ochoa
+****************************************************************************/
+
+#ifndef HELPER_HPP
+#define HELPER_HPP
+
+#include <string>
+#include <vector>
+
+class Helper {
+    public:
+        static void transform_string(std::string stringIn, std::vector<int> &v);
+        static void print_vector(std::vector<int> v);
+        static std::vector<int> generate_random_ints(int size_n);
+		static float fraction_to_float(std::string fraction);
+};
+#endif
