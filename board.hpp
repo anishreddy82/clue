1da13820492766d0f8a225735d86140029e08854
Kevin Benoit <kevin.benoit8@gmail.com>
2020-05-06 11:56:15 -0600

    added board coordinate system for player locations

--- board.hpp
+++ board.hpp
@@ -4,14 +4,19 @@
 #include <cstdlib>
 #include <ctime>
 #include <string>
+#include <utility>
 #include "room.hpp"
 #include "Card.hpp"
 
 class Board{
 private:
-	
+	int w;
+	int h;
+	std::vector<std::pair<int, int>> player_coords;
 public:
+	Board();
+
 	void dif(std::vector<Player>);
-  Room **boardArray;
+	Room **boardArray;
 	std::vector<Card> murderCards;
 };
